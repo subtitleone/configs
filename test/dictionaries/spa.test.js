a4c355506cc4cb706ca21c44ac8e5eca2092cd85
Stephen Anwar <stephen.anwar@gmail.com>
2024-11-22 01:02:16 +1100

    feat(Dictionary): Added Spanish rules & tests

---
+++ test/dictionaries/spa.test.js
@@ -0,0 +1,22 @@
+import { loadDictionary, getDescription, getRule } from './utils'
+
+describe('Spanish dictionary', () => {
+  const dict = loadDictionary('spa')
+
+  it(getDescription(dict, 0), () => {
+    const rule = getRule(dict, 0)
+    expect('-SÍ. A'.replace(rule.regex, rule.replace)).toEqual('-Sí. A')
+    expect('SÍ'.replace(rule.regex, rule.replace)).toEqual('Sí')
+    expect('SÍ '.replace(rule.regex, rule.replace)).toEqual('Sí ')
+    expect('SÍA'.replace(rule.regex, rule.replace)).toEqual('SÍA')
+    expect('-¿SÍ? '.replace(rule.regex, rule.replace)).toEqual('-¿Sí? ')
+  })
+
+  it(getDescription(dict, 1), () => {
+    const rule = getRule(dict, 1)
+    expect('¿ Turner?'.replace(rule.regex, rule.replace)).toEqual('¿Turner?')
+    expect('¿Turner  ?'.replace(rule.regex, rule.replace)).toEqual('¿  Turner  ?')
+    expect('¿ abc? ¿abc ?'.replace(rule.regex, rule.replace)).toEqual('¿abc? ¿ abc ?')
+    expect('¿ abc? ¿abc ? ¿abc   ?'.replace(rule.regex, rule.replace)).toEqual('¿abc? ¿ abc ? ¿   abc   ?')
+  })
+})
