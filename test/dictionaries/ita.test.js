ee83d8e815fd71109420564f792c35bf1b6ee9a9
Stephen Anwar <stephen.anwar@gmail.com>
2024-12-02 21:23:38 +1100

    feat(Italian): Add new rule

--- test/dictionaries/ita.test.js
+++ test/dictionaries/ita.test.js
@@ -6,17 +6,24 @@ describe('Italian dictionary', () => {
   it(getDescription(dict, 0), () => {
     const rule = getRule(dict, 0)
     expect('| tuoi '.replace(rule.regex, rule.replace)).toEqual('I tuoi ')
+    expect('|l divo '.replace(rule.regex, rule.replace)).toEqual('Il divo ')
   })
 
   it(getDescription(dict, 1), () => {
     const rule = getRule(dict, 1)
-    expect('SÌ'.replace(rule.regex, rule.replace)).toEqual('Sì')
-    expect('SÌ.'.replace(rule.regex, rule.replace)).toEqual('Sì.')
-    expect('-SÌ.'.replace(rule.regex, rule.replace)).toEqual('-Sì.')
+    expect('Forzal!'.replace(rule.regex, rule.replace)).toEqual('Forza!')
   })
 
   it(getDescription(dict, 2), () => {
     const rule = getRule(dict, 2)
-    expect('Forzal!'.replace(rule.regex, rule.replace)).toEqual('Forza!')
+    expect('-SÌì.'.replace(rule.regex, rule.replace)).toEqual('-Sì.')
+    expect('-SÌì'.replace(rule.regex, rule.replace)).toEqual('-Sì')
+    expect(' SÌì '.replace(rule.regex, rule.replace)).toEqual(' Sì ')
+    expect('SÌì '.replace(rule.regex, rule.replace)).toEqual('Sì ')
+    expect('SÌì'.replace(rule.regex, rule.replace)).toEqual('Sì')
+    expect('SÌì.'.replace(rule.regex, rule.replace)).toEqual('Sì.')
+    expect('SÌ, è vero.'.replace(rule.regex, rule.replace)).toEqual('Sì, è vero.')
+    expect('-SÌ.'.replace(rule.regex, rule.replace)).toEqual('-Sì.')
+    expect('SÌ.'.replace(rule.regex, rule.replace)).toEqual('Sì.')
   })
 })
